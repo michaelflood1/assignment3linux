cc890ebd109db4875d7755e84bb7da3bef129694
Michael Flood <156374637+michaelflood1@users.noreply.github.com>
2024-11-27 21:14:44 -0800

    Update createuser.sh

--- createuser.sh
+++ createuser.sh
@@ -1,5 +1,10 @@
+sudo groupadd nginx
+
 sudo useradd -r -d /var/lib/webgen --group nginx --shell /usr/bin/nologin webgen
-# creates system user with above directory and creates or add into a group for it 
+# creates system user with above directory and add into group for it 
+
+sudo mkdir -p /var/lib/webgen/bin
+sudo mkdir -p /var/lib/webgen/HTML
 
 sudo chown -R webgen:webgen /var/lib/webgen
 sudo chmod 700 /var/lib/webgen
